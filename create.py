9dea0f0654b7ac84e4205b7e836c1728b8647a8e
andrelu1 <andre.mastrocola@fatec.sp.gov.br>
2023-03-01 12:28:07 -0300

    Add files via upload

---
+++ create.py
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+
+import mysql.connector
+import datetime
+
+connection = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="toor",
+  database="teste"
+)
+
+cursor = connection.cursor()
+
+sql = "INSERT INTO users (name, email, created) VALUES (%s, %s, %s)"
+data = (
+  'Primeiro Usuário',
+  'primeirousuario@teste.com.br',
+  datetime.datetime.today()
+)
+
+cursor.execute(sql, data)
+connection.commit()
+
+userid = cursor.lastrowid
+
+cursor.close()
+connection.close()
+
+print("Foi cadastrado o novo usuário de ID:", userid)
+
+#Tutorial Seguido em : https://irias.com.br/blog/python-mysql-criando-um-crud-completo/
